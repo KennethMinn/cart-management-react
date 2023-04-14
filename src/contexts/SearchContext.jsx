30d45dab52b155ac00c183ee51bb78d46ef84f90
KennethMinn <minthukha2472005@gmail.com>
2023-04-14 10:54:56 +0630

    updated with useReducer hook instead of state hook

--- src/contexts/SearchContext.jsx
+++ src/contexts/SearchContext.jsx
@@ -1,9 +1,37 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer, useState } from "react";
 
 export const SearchContext = createContext();
 
+const SEARCH_ACTION_TYPES = {
+  SET_SEARCH_FIELD: "SET_SEARCH_FIELD",
+};
+
+const INITIAL_STATE = {
+  searchField: "",
+};
+
+const searchReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case SEARCH_ACTION_TYPES.SET_SEARCH_FIELD:
+      return {
+        ...state,
+        searchField: payload,
+      };
+
+    default:
+      throw new Error(`unhandled error type of ${type}`);
+  }
+};
+
 export const SearchProvider = ({ children }) => {
-  const [searchField, setSearchField] = useState("");
+  // const [searchField, setSearchField] = useState("");
+  const [{ searchField }, dispatch] = useReducer(searchReducer, INITIAL_STATE);
+
+  const setSearchField = (value) => {
+    dispatch({ type: SEARCH_ACTION_TYPES.SET_SEARCH_FIELD, payload: value });
+  };
+
   const value = { searchField, setSearchField };
 
   return (
