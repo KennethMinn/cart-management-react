6acb6acaebf9b01030457b0455a0e48f1725e1e1
KennethMinn <minthukha2472005@gmail.com>
2023-04-22 22:38:31 +0630

    updated from redux flow to redux toolkit

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,16 +1,16 @@
-import React, { useContext, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import Cards from "../components/Cards";
-import { selectSearchField } from "../store/search/search-selector";
+import React, { useContext, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Cards from '../components/Cards';
+import { selectSearchField } from '../store/search/search-selector';
 import {
   setProducts,
   setFilteredProducts,
-} from "../store/products/product-reducer";
+} from '../store/products/product-reducer';
 import {
   selectProducts,
   selectFilteredProducts,
-} from "../store/products/product-selector";
-import { setCategories } from "../store/categories/category-action";
+} from '../store/products/product-selector';
+import { setCategories } from '../store/categories/category-reducer';
 
 const Home = () => {
   const products = useSelector(selectProducts);
@@ -26,25 +26,25 @@ const Home = () => {
   }, []);
 
   useEffect(() => {
-    const newFilteredProducts = products.filter((product) =>
+    const newFilteredProducts = products.filter(product =>
       product.title.toLowerCase().includes(searchField)
     );
     dispatch(setFilteredProducts(newFilteredProducts));
   }, [searchField, products]);
 
   const getProducts = async () => {
-    const res = await fetch("https://fakestoreapi.com/products");
+    const res = await fetch('https://fakestoreapi.com/products');
     const data = await res.json();
     dispatch(setProducts(data));
     const uniqueCategories = [
-      ...new Set(data.map((product) => product.category)),
+      ...new Set(data.map(product => product.category)),
     ];
     dispatch(setCategories(uniqueCategories));
   };
 
   return (
     <div className=" grid justify-items-center grid-cols-3 gap-44 gap-y-10 ">
-      {filteredProducts?.map((product) => (
+      {filteredProducts?.map(product => (
         <Cards key={product.id} product={product} id={product.id} />
       ))}
     </div>
--- src/store/categories/category-reducer.js
+++ src/store/categories/category-reducer.js
@@ -1,19 +1,32 @@
-import { CATEGORIES_ACTION_TYPES } from "./category-type";
+import { createSlice } from '@reduxjs/toolkit';
 
 const INITIAL_STATE = {
   categories: [],
 };
 
-export const categoriesReducer = (state = INITIAL_STATE, action = {}) => {
-  const { type, payload } = action;
-  switch (type) {
-    case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
-      return {
-        ...state,
-        categories: payload,
-      };
+export const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState: INITIAL_STATE,
+  reducers: {
+    setCategories(state, action) {
+      state.categories = action.payload;
+    },
+  },
+});
 
-    default:
-      return state;
-  }
-};
+export const { setCategories } = categoriesSlice.actions;
+export const categoriesReducer = categoriesSlice.reducer;
+
+// export const categoriesReducer = (state = INITIAL_STATE, action = {}) => {
+//   const { type, payload } = action;
+//   switch (type) {
+//     case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
+//       return {
+//         ...state,
+//         categories: payload,
+//       };
+
+//     default:
+//       return state;
+//   }
+// };
