898f6b2eab0723155f7586aab7bdf85564e016fe
KennethMinn <minthukha2472005@gmail.com>
2023-06-01 02:42:42 -0400

    added loading to detail page

--- src/routes/Details.jsx
+++ src/routes/Details.jsx
@@ -1,10 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { excerpt } from "../utils/helper-func";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setIsLoading } from '../store/products/product-reducer';
+import { selectIsLoading } from '../store/products/product-selector';
+import { useParams } from 'react-router-dom';
+import { excerpt } from '../utils/helper-func';
+import ClimbingBoxLoader from 'react-spinners/ClimbingBoxLoader';
 
 const Details = () => {
   const { id } = useParams();
   const [details, setDetails] = useState({});
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
 
   useEffect(() => {
     getDetails();
@@ -12,17 +18,25 @@ const Details = () => {
 
   const excerpt = (str, maxLength = 70) => {
     if (str?.length >= maxLength) {
-      return str.substr(0, maxLength) + "...";
+      return str.substr(0, maxLength) + '...';
     }
     return str;
   };
 
   const getDetails = async () => {
+    dispatch(setIsLoading(true));
     const res = await fetch(`https://fakestoreapi.com/products/${id}`);
     const data = await res.json();
-    console.log(data);
     setDetails(data);
+    dispatch(setIsLoading(false));
   };
+
+  if (isLoading)
+    return (
+      <div className=" flex h-screen items-center justify-center">
+        <ClimbingBoxLoader color="#36d7b7" cssOverride={{}} size={20} />
+      </div>
+    );
 
   return (
     <div className=" flex justify-between items-center min-h-screen">
